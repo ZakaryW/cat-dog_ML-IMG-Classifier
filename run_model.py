f33fc4a300f851657f35c27016bba8cb60aa71bf
ZakaryW <wattsz@oregonstate.edu>
2023-09-24 01:10:35 -0700

    Trained model off greater data quantity to improve accuracy

--- run_model.py
+++ run_model.py
@@ -6,15 +6,30 @@ import cv2
 
 gpus = tf.config.experimental.list_physical_devices('GPU')
 for gpu in gpus:
-    tf.config.experimental.set_memory_growth(gpu, True)
+    tf.config.set_memory_growth(gpu, True)
 
-img = cv2.imread('dogtest.jpg')
-resize = tf.image.resize(img, (256,256))
+def evaluate_prediction(p_val):
+    if p_val > 0.5:
+        print(f'Predicted as class: Dog')
+    else:
+        print(f'Predicted as class: Cat')
+    return
 
-model = models.load_model(os.path.join('model', 'cat-dog-classify_model.h5'))
-yhat = model.predict(np.expand_dims(resize/255, 0))
+def load_active_img(filename):
+    img = cv2.imread(filename)
+    resize = tf.image.resize(img, (256,256))
+    return resize
 
-if yhat > 0.5:
-    print(f'Predicted as a DOG')
-else:
-    print(f'Predicted as a CAT')
+model = models.load_model(os.path.join('model', 'cat-dog-classify_model.keras'))
+
+filename = 'dogtest.jpg'
+image = load_active_img(filename)
+yhat = model.predict(np.expand_dims(image/255, 0))
+evaluate_prediction(p_val=yhat)
+
+filename = 'cattest.jpg'
+image = load_active_img(filename)
+yhat = model.predict(np.expand_dims(image/255, 0))
+evaluate_prediction(p_val=yhat)
+    
+exit('Exited')
